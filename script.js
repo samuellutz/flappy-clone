37bd20b8805a95ea2f094c9fc3515bdfef77d06c
samuel lutz <samlutz89@gmail.com>
2022-07-17 09:21:11 -0700

    setting animation timing

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ document.addEventListener("keypress",handleStart,{once : true})
 
 const title = document.querySelector("[data-title]")
 
+
+let lastTime
 function updateLoop(time) {
+    console.log(time - lastTime);
+    lastTime = time
     window.requestAnimationFrame(updateLoop)
 }
 
