ecc7a548b8a5bbfec64a938c2e1045f1d567eac4
samuel lutz <samlutz89@gmail.com>
2022-07-21 14:50:31 -0700

    added lose function for touching top or bottom

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import {updateBird, setupBird, } from './bird.js'
+import {updateBird, setupBird, getbirdRect} from './bird.js'
 
 
 
@@ -17,8 +17,15 @@ function updateLoop(time) {
     }
     const delta = time - lastTime
     updateBird(delta)
+    if (checkLose()) return handleLose()
     lastTime = time
     window.requestAnimationFrame(updateLoop)
+}
+
+function checkLose() {
+    const birdRect = getbirdRect()
+    const outsideWorld = birdRect.top < 0 || birdRect.bottom > window.innerHeight
+    return outsideWorld
 }
 
 function handleStart() {
